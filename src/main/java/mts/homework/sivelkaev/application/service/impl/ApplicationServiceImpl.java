11de2f7be37cdcdc98b0d4ec9cd741f950d066ad
gasivelkay <gasivelkay@mtsbank.ru>
2024-07-21 18:53:14 +0300

    4 Реализована отправка заявки во флоу-сервис

--- src/main/java/mts/homework/sivelkaev/application/service/impl/ApplicationServiceImpl.java
+++ src/main/java/mts/homework/sivelkaev/application/service/impl/ApplicationServiceImpl.java
@@ -3,6 +3,9 @@ package mts.homework.sivelkaev.application.service.impl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import mts.homework.sivelkaev.application.controller.dto.CreateApplicationRequest;
+import mts.homework.sivelkaev.application.helper.JsonMarshallingHelper;
+import mts.homework.sivelkaev.application.kafka.KafkaProducerMethod;
+import mts.homework.sivelkaev.application.kafka.request.ApplicationStartFlowRequest;
 import mts.homework.sivelkaev.application.kafka.request.UpdateApplicationStatusRequest;
 import mts.homework.sivelkaev.application.model.entity.ApplicationEntity;
 import mts.homework.sivelkaev.application.model.repository.ApplicationRepository;
@@ -20,6 +23,8 @@ public class ApplicationServiceImpl implements ApplicationService {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     private final ApplicationRepository applicationRepository;
+    private final JsonMarshallingHelper jsonMarshallingHelper;
+    private final KafkaProducerMethod kafkaProducerMethod;
 
     @Override
     public String createApplication(CreateApplicationRequest req) {
@@ -34,6 +39,19 @@ public class ApplicationServiceImpl implements ApplicationService {
                 .passportNumber(req.getPassportNumber())
                 .build();
         applicationRepository.save(applicationEntity);
+
+        ApplicationStartFlowRequest startFlowReq = ApplicationStartFlowRequest.builder()
+                .id(applicationEntity.getId())
+                .type(applicationEntity.getType())
+                .status(applicationEntity.getStatus())
+                .firstName(applicationEntity.getFirstName())
+                .middleName(applicationEntity.getMiddleName())
+                .lastName(applicationEntity.getLastName())
+                .birthDate(applicationEntity.getBirthDate().format(FORMATTER))
+                .passportNumber(applicationEntity.getPassportNumber())
+                .build();
+        var message = jsonMarshallingHelper.marshall(startFlowReq);
+        kafkaProducerMethod.send("application.flow.start", message);
 
         return "Создана заявка " + applicationEntity.getId() + " на открытие счета.";
     }
